5ed7131deab880b5d97b313d85b6d52a222df765
kim seung hoon <kim seung hoon@kimseunghoon-PC>
2016-12-26 14:45:34 +0900

    asd

---
+++ asd/src/asd/asd.java
@@ -0,0 +1,20 @@
+package asd;
+import java.util.Scanner;
+public class asd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   Scanner s=new Scanner(System.in);
+           System.out.print("입력하세요:");
+            int b=s.nextInt();
+            
+           for (int i = 0; i < b; i++) {
+     		   for (int j = 0; j < b; j++) {
+     		    System.out.print("*");
+     		   }
+     		   System.out.println(" ");
+     		  }
+     	        	
+	}
+
+}
